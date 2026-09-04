4680588d07fbef74988274a9fa8d683a73bc3bb1
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-09-04 14:01:47 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello, ${person}, How are you? `;
+}
+const username = "John";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greet(person:string): string{
+    return`Hello, ${person}, How are you? `;
+}
+const username: string = "John";
+console.log(greet(username));
