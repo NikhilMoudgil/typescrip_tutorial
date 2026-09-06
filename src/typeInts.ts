faa3b490c5714fdb1f6a048c95e98327721b65ba
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-09-06 20:59:34 +0530

    Types IN ts

---
+++ src/typeInts.ts
@@ -0,0 +1,15 @@
+let channel = "solo";
+channel = "SoloSync";
+let cups = Math.floor(Math.random() > 0.5 ? 10 : 5);
+
+//type error
+let channelName="Nikhil.Builds"
+
+//channelName = 11232; //TYPE ERROR
+
+//ANNOTATION
+let channelName2: string = "Nikhil.Builds";
+channelName2 = "Nikhil.Builds2";
+
+let solosync: boolean// boolean type annotation
+solosync = true;
